1e3075ba067d3826dcb79ffdc6764ac3602cc278
DevHars <vargassanchezfabio01@gmail.com>
2022-08-09 15:24:33 -0500

    re definition

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+//
+var firstName; //undefine, no hay nada asignado
+firstName = 'Fabio';
+console.log(firstName);
+
+var lastName = 'David'; //declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; // declarando / asignanndo
+var secondName = 'Ana'; // reasignando
+console.log(secondName); 
+
+
+// Let
+let fruit = 'Apple'; // decalrar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+let fruit = 'Banana'; // no es posible redeblarar con let
+
+
+// Const
+const animal = 'Dog'; // decalrar y asignar
+animal = 'cat'; //no se puede redeclarar
+console.log(animal);
+
+// Sugun el uso que se tenga se puede trabajar
+const vehicles = [];
+vehicles.push('car'); // se anade al arreglo
+console.log(vehicles);
+
+vehicles.pop(); // se elimina del arreglo
+console.log(vehicles);
